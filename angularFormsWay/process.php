47604bbfeff959f3102557a7440d8301f664436a
Ryan Jung <rjung@boardvantage.com>
2014-05-20 14:19:20 -0700

    php

--- angularFormsWay/process.php
+++ angularFormsWay/process.php
@@ -27,43 +27,26 @@ $data           = array();      // array to pass back data
         $data['success'] = true;
         $data['message'] = 'Success!';
 
-         $to = "rjung@boardvantage.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $name = $_POST['name'];
-    $superheroAlias = $_POST['superheroAlias'];
-    $subject = "Form submission";
-    $subject2 = "Copy of your form submission";
-    $message = $name . " wrote the following:" . "\n\n" . $_POST['superheroAlias'];
-    $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['superheroAlias'];
+        $to = "rjung@boardvantage.com"; // this is your Email address
+        $from = $_POST['email']; // this is the sender's Email address
+        $name = $_POST['name'];
+        $superheroAlias = $_POST['superheroAlias'];
+        $subject = "Form submission";
+        $subject2 = "Copy of your form submission";
+        $message = $name . " wrote the following:" . "\n\n" . $_POST['superheroAlias'];
+        $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['superheroAlias'];
 
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
+        $headers = "From:" . $from;
+        $headers2 = "From:" . $to;
+        mail($to,$subject,$message,$headers);
+        mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+        // echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
+        // You can also use header('Location: thank_you.php'); to redirect to another page.
+        // You cannot use header and echo together. It's one or the other.
 
     }
 
     // return all our data to an AJAX call
     echo json_encode($data);
-
-if(isset($_POST['submit'])){
-    $to = "rjung@boardvantage.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $name = $_POST['name'];
-    $superheroAlias = $_POST['superheroAlias'];
-    $subject = "Form submission";
-    $subject2 = "Copy of your form submission";
-    $message = $name . " wrote the following:" . "\n\n" . $_POST['superheroAlias'];
-    $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['superheroAlias'];
-
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    // You cannot use header and echo together. It's one or the other.
-    }
 
 ?>
